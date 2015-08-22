7d13d015dd72dd38525b6d58e3f72ea64cb06e64
Matt Cass <matthewcasserly@gmail.com>
2015-08-21 23:12:56 -0400

    remove off screen nav

--- source/js/script.js
+++ source/js/script.js
@@ -2,36 +2,34 @@
 $(document).ready(function($) {
 
     // WELCOME MESSAGE TO ANY DEVELOPERS
-    console.log("Well hello there.");
-
-    // MOBILE NAVIGATION
-    $('.js-click').click(function() {
-        $(".nav").toggleClass('active');
-        $(".menu-link").toggleClass('active');
-        $("main").toggleClass('active');
-    });
+    console.log("Sup console people.");
 
     // FILTER JOURNAL ENTRIES BY CATEGORY
-    $('.filter').delegate('a', 'click', function (event) {
-    $('.content').hide().filter('.' + this.href.slice(this.href.indexOf("#") + 1)).show();
-        event.preventDefault();
+    $('.filter').delegate('a', 'click', function (e) {
+        e.preventDefault();
+
+        $('.content').hide().filter('.' + this.href.slice(this.href.indexOf("#") + 1)).show();
     }); 
 
     // SMOOTH SCROLLING
     $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        $('a[href*=#]:not([href=#])').on('click', function() {
+
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
             var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                    if (target.length) {
-                        $('html,body').animate({
-                            scrollTop: target.offset().top - 80
-                        }, 1000);
+
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 80
+                    }, 1000);
+
                     return false;
-                    }
+                }
             }
         });
     });	
 
-// CLOSE
+// end file
 });
