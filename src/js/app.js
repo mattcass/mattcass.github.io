b4e0fd8fb4066f13d39eca3d3d4f7ecc43e3eed2
Matt Cass <matthewcasserly@gmail.com>
2016-11-04 20:54:09 -0400

    update router

--- src/js/app.js
+++ src/js/app.js
@@ -12,22 +12,6 @@ const app = document.getElementById('app');
 // Set hash query param to false
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
-appHistory.listen(location => {
-  // Use setTimeout to make sure this runs after React Router's own listener
-  setTimeout(() => {
-    // Keep default behavior of restoring scroll position when user:
-    // - clicked back button
-    // - clicked on a link that programmatically calls `history.goBack()`
-    // - manually changed the URL in the address bar (here we might want
-    // to scroll to top, but we can't differentiate it from the others)
-    if (location.action === 'POP') {
-      return;
-    }
-    // In all other cases, scroll to top
-    window.scrollTo(0, 0);
-  });
-});
-
 // Import Components
 import Index from './components/index.js';
 import Blog from './components/blog.js';
