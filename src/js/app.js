6ed041f21957702847cad82e00412a0c23c01551
Matt Cass <matthewcasserly@gmail.com>
2016-11-04 20:39:46 -0400

    remove query param from hashHistory

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,13 @@ import '../css/master.scss';
 // Import React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { IndexRoute, Router, Route, Link, hashHistory } from 'react-router';
+import { IndexRoute, Router, Route, useRouterHistory  } from 'react-router';
+import { createHashHistory } from 'history';
 
 // Define root element
 const app = document.getElementById('app');
+// Set hash query param to false
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
 // Import Components
 import Index from './components/index.js';
@@ -17,7 +20,7 @@ import NotFound from './components/404.js';
 
 // Routing
 ReactDOM.render(
-  <Router history={hashHistory}>
+  <Router onUpdate={() => window.scrollTo(0, 0)} history={appHistory}>
     <Route path="/" component={Index}>
       <IndexRoute component={Writing} />
       <Route path="/blog(/:article)" component={Blog} />
