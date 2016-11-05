83c343c51dce0b9502c95e4eb57c1c8881530611
Matt Cass <matthewcasserly@gmail.com>
2016-11-04 20:52:26 -0400

    update router

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,22 @@ const app = document.getElementById('app');
 // Set hash query param to false
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
+appHistory.listen(location => {
+  // Use setTimeout to make sure this runs after React Router's own listener
+  setTimeout(() => {
+    // Keep default behavior of restoring scroll position when user:
+    // - clicked back button
+    // - clicked on a link that programmatically calls `history.goBack()`
+    // - manually changed the URL in the address bar (here we might want
+    // to scroll to top, but we can't differentiate it from the others)
+    if (location.action === 'POP') {
+      return;
+    }
+    // In all other cases, scroll to top
+    window.scrollTo(0, 0);
+  });
+});
+
 // Import Components
 import Index from './components/index.js';
 import Blog from './components/blog.js';
@@ -20,7 +36,7 @@ import NotFound from './components/404.js';
 
 // Routing
 ReactDOM.render(
-  <Router onUpdate={() => window.scrollTo(0, 0)} history={appHistory}>
+  <Router history={appHistory}>
     <Route path="/" component={Index}>
       <IndexRoute component={Writing} />
       <Route path="/blog(/:article)" component={Blog} />
